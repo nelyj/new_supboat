eea987573ec26d17300d2b837cdf607fd35a16f8
Nelson Pato Jiménez <developer.jimenez@gmail.com>
2013-05-23 15:55:02 -0400

    esta es la informacion que puedo recuperar de meetup.com

--- Gemfile
+++ Gemfile
@@ -21,5 +21,6 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
+gem 'httparty'
 gem 'jquery-rails'
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @response = HTTParty.get('http://api.meetup.com/2/events/?status=upcoming&order=time&limited_events=False&group_urlname=startupchile&desc=false&offset=0&format=json&page=20&fields=&sig_id=12911163&sig=014424a15746b3acbbd04395abc44da6d1750774')
+    @response = @response["results"]
   end
 end
