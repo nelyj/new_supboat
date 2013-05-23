3dc8b7434e895dc57238882d5301c18edd15bcf1
Nelson Pato Jiménez <developer.jimenez@gmail.com>
2013-05-23 12:46:58 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newsupboat::Application.config.secret_token = 'f13784b264e2fb4fb35a45837fb6284271e08a1ad9f2d3212c804e51b8d729d352fcae11bf1c9c547acf64acc4071cdea3a40ce7b4addc2b73b7f28cf22601f2'
