c95bf9f6bd703aa6f25d9f4fab5697ec5569f520
Nelson Pato Jiménez <developer.jimenez@gmail.com>
2013-06-17 09:53:07 -0400

    Se actualiza Asia

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,6 +12,10 @@ class HomeController < ApplicationController
     @europa = HTTParty.get('http://api.meetup.com/2/events?status=upcoming&event_id=124135132%2C121029312%2C124112762&order=time&limited_events=False&desc=false&offset=0&format=json&page=20&fields=&sig_id=12911163&sig=e3d2f651481f0da1d4a6c0d9ff87f5cc94799c10')
     @europa = @europa["results"]
 
+    @asia = HTTParty.get('http://api.meetup.com/2/events?status=upcoming&event_id=122051032&order=time&limited_events=False&desc=false&offset=0&format=json&page=20&fields=&sig_id=12911163&sig=e4c810fe20ab3fb9994e877b142b1b74fab052f7')
+    @asia = @asia["results"]
+
+
 
   end
 end
