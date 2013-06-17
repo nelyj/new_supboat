1b9fec872eb69117ece9950516a3ae22d7a7a607
Nelson Pato Jiménez <developer.jimenez@gmail.com>
2013-06-17 09:38:54 -0400

    Se actualizan datos para europa

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,9 @@ class HomeController < ApplicationController
       country["venue"]["country"] = u.name
     end
 
-    @norte =HTTParty.get('http://api.meetup.com/2/events?status=upcoming&event_id=121039142&order=time&limited_events=False&group_urlname=startupchile&desc=false&offset=0&format=json&page=20&fields=&sig_id=12911163&sig=a92dca4ee46971de8b1016dcaf81d79b3b893ae4')
+    @europa = HTTParty.get('http://api.meetup.com/2/events?status=upcoming&event_id=124135132%2C121029312%2C124112762&order=time&limited_events=False&desc=false&offset=0&format=json&page=20&fields=&sig_id=12911163&sig=e3d2f651481f0da1d4a6c0d9ff87f5cc94799c10')
+    @europa = @europa["results"]
+
 
   end
 end
